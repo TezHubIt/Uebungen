5edc260ddb6d169fa63c953c5e824038a621ffe1
TezHubIT <it.tezcanli@gmail.com>
2025-05-15 15:10:40 +0200

    X

--- 3 Einfache Rechenaufgabe/3 Einfache Rechenaufgabe/Program.cs
+++ 3 Einfache Rechenaufgabe/3 Einfache Rechenaufgabe/Program.cs
@@ -6,3 +6,16 @@ int zahl2 = int.Parse(Console.ReadLine());
 
 int ergebnis = zahl1 + zahl2;
 Console.WriteLine("Das Ergebnis ist: " + ergebnis);
+
+
+Console.WriteLine("Gib die erste Zahl ein:");
+string eingabe1 = Console.ReadLine();
+int zahl3 = Convert.ToInt32(eingabe1);
+
+Console.WriteLine("Gib die zweite Zahl ein:");
+string eingabe2 = Console.ReadLine();
+int zahl4 = Convert.ToInt32(eingabe2);
+
+int ergebnis2 = zahl3 + zahl4;
+Console.WriteLine("Das Ergebnis ist: " + ergebnis2);
+
--- 4 Fläche des Rechtecks/4 Fläche des Rechtecks/Program.cs
+++ 4 Fläche des Rechtecks/4 Fläche des Rechtecks/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Geben Sie die Länge des Rechtecks ein:");
+int length = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Geben Sie die Breite des Rechtecks ein:");
+int width = Convert.ToInt32(Console.ReadLine());
+
+int area = length * width;
+Console.WriteLine($"Die Fläche des Rechtecks beträgt: {area}");
+
