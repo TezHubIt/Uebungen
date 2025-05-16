05d5e1543aa650c579211dc83700f3f4c7287664
TezHubIT <it.tezcanli@gmail.com>
2025-05-16 15:16:15 +0200

    Aufgaben bearbeitet

---
+++ 22 Fakultät berechnen/22 Fakultät berechnen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Numerics;
+
+Console.WriteLine("Gebe die Zahl ein: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+BigInteger fakultät = 1;
+for (int i = 1; i <= n; i++)
+{
+    fakultät *= i;
+}
+Console.WriteLine($"Die Fakultät von {n} ist {fakultät}.");
+
---
+++ 23 Primzahlprüfung/23 Primzahlprüfung/Program.cs
@@ -0,0 +1,29 @@
+﻿using static System.Net.WebRequestMethods;
+
+Console.WriteLine("Gib eine Zahl ein:");
+int zahl = Convert.ToInt32(Console.ReadLine());
+bool istPrim = true;
+
+if (zahl <= 1)
+{
+    istPrim = false;
+}
+else
+{
+    for (int i = 2; i <= Math.Sqrt(zahl); i++)          // Optimierung: Nur bis zur Quadratwurzel der Zahl prüfen
+    {                                                   //  Warum bis zur Quadratwurzel? Wenn eine Zahl zahl einen Teiler
+                                                        //  größer als ihre Quadratwurzel hätte, müsste der andere Teiler
+                                                        //  kleiner als die Quadratwurzel sein.Deshalb reicht es,
+                                                        //  nur bis zur Quadratwurzel zu prüfen – das spart Rechenzeit.
+        if (zahl % i == 0)
+        {
+            istPrim = false;
+            break;
+        }
+    }
+}
+
+if (istPrim)
+    Console.WriteLine(zahl + " ist eine Primzahl.");
+else
+    Console.WriteLine(zahl + " ist keine Primzahl.");
