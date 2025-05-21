f9c9f79d78fffb616229f1532de372c7e85f95d2
TezHubIt <it.tezcanli@gmail.com>
2025-05-21 15:14:17 +0200

    Buch Lesen Übungen S 152

---
+++ 00 - Buch/00 - Buch/Program.cs
@@ -0,0 +1,20 @@
+﻿// Beispiel: ..\Kapitel 2\LogischeOperatoren
+class Program
+{
+    static void Main(string[] args)
+    {
+        int x = 8;
+        int y = 8;
+        // wenn die Bedingung wahr ist, dann dies durch eine
+        // Ausgabe an der Konsole bestätigen
+        if ((x != y) || DoSomething())
+            Console.WriteLine("Bedingung ist erfüllt");
+        Console.ReadLine();
+    }
+    // benutzerdefinierte Methode
+    static bool DoSomething()
+    {
+        Console.WriteLine("in DoSomething");
+        return true;
+    }
+}
