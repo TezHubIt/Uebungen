7f1c211728a3aeefa82e3066693312cfc5eded75
TezHubIT <it.tezcanli@gmail.com>
2025-05-22 12:59:19 +0200

    Release erstellt

---
+++ 1033 Zahlensystem Konvertieren/1033 Zahlensystem Konvertieren/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _1033_Zahlensystem_Konvertieren
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ 1033 Zahlensystem Konvertieren/1033 Zahlensystem Konvertieren/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace DezimalUmwandler
+{
+    public partial class MainWindow : Window
+    {
+        private bool sollTextfeldGeleertWerden = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+         }
+
+        private void textBoxDezimal_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (sollTextfeldGeleertWerden)
+            {
+                textBoxDezimal.Clear();
+                sollTextfeldGeleertWerden = false;
+            }
+
+            if (e.Key == Key.Enter)
+            {
+                Umwandeln();
+                e.Handled = true; // verhindert "Ding"-Sound bei Enter
+            }
+            
+
+        }
+
+
+
+
+        private void Umwandeln()
+        {
+            string input = textBoxDezimal.Text;
+
+            if (int.TryParse(input, out int zahl))
+            {
+                string binaer = Convert.ToString(zahl, 2);
+
+                // Auf ein Vielfaches von 4 auffüllen
+                int padding = 4 - (binaer.Length % 4);
+                if (padding != 4)
+                {
+                    binaer = binaer.PadLeft(binaer.Length + padding, '0');
+                }
+
+                // 4er-Gruppierung
+                string gruppiert = "";
+                for (int i = 0; i < binaer.Length; i += 4)
+                {
+                    if (i > 0) gruppiert += " ";
+                    gruppiert += binaer.Substring(i, 4);
+                }
+
+                textBlockBinaer.Text = $"Binär: {gruppiert}";
+                textBlockHex.Text = $"Hexadezimal: {Convert.ToString(zahl, 16).ToUpper()}";
+                sollTextfeldGeleertWerden = true;
+
+            }
+            else
+            {
+                MessageBox.Show("Bitte eine gültige ganze Zahl eingeben.", "Ungültige Eingabe", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            
+        }
+    }
+}
