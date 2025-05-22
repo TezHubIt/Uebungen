aae6fd231f19077ad082b942c8d93a98adc403af
TezHubIT <it.tezcanli@gmail.com>
2025-05-22 09:32:26 +0200

    Bitweise Operatoren

--- 00 - Buch/00 - Buch/Program.cs
+++ 00 - Buch/00 - Buch/Program.cs
@@ -1,10 +1,25 @@
-﻿int flags = 0b_0000_0101; // Bits 0 und 2 gesetzt
+﻿using System;
 
-// Bit 1 setzen
-flags |= (1 << 1); // Ergebnis: 0b_0000_0111
+class Program
+{
+    static void Main()
+    {
+        int flags = 0b_0000_0101; // 5 in Dezimal, Bits 0 und 2 gesetzt
+        Console.WriteLine("Start:       " + Convert.ToString(flags, 2).PadLeft(8, '0'));
 
-// Bit 2 löschen
-flags &= ~(1 << 2); // Ergebnis: 0b_0000_0011
+        // Bit 1 setzen
+        flags |= (1 << 6);
+        Console.WriteLine("Bit 1 setzen:" + Convert.ToString(flags, 2).PadLeft(8, '0'));
 
-// Bit 0 umkehren (toggle)
-flags ^= (1 << 0); // Ergebnis: 0b_0000_0010
+        // Bit 2 löschen
+        flags &= ~(1 << 6);
+        Console.WriteLine("Bit 2 löschen:" + Convert.ToString(flags, 2).PadLeft(8, '0'));
+
+        // Bit 0 umkehren (toggle)
+        flags ^= (1 << 6);
+        Console.WriteLine("Bit 0 toggle:" + Convert.ToString(flags, 2).PadLeft(8, '0'));
+
+        // Endwert auch dezimal anzeigen
+        Console.WriteLine("Ergebnis (dezimal): " + flags);
+    }
+}
