db1d19f01b8f1c3faf06a082c7fdff2447283474
TezHubIt <it.tezcanli@gmail.com>
2025-05-23 11:11:50 +0200

    Aufgaben bearbeitet

---
+++ 34 Rotation in einem Array/34 Rotation in einem Array/Program.cs
@@ -0,0 +1,25 @@
+﻿string[] buchstaben = { "A", "B", "C", "D", "E", "F", "G" };
+
+Console.WriteLine("Vor der Rotation:");
+foreach (string buchstabe in buchstaben)
+{
+    Console.Write(buchstabe + " ");
+}
+
+RotateRight(buchstaben);
+
+Console.WriteLine("\nNach der Rotation:");
+foreach (string buchstabe in buchstaben)
+{
+    Console.Write(buchstabe + " ");
+}
+
+static void RotateRight(string[] arr)
+{
+    string tmp = arr[arr.Length - 1]; // Letztes Element speichern
+    for (int i = arr.Length - 1; i > 0; i--)
+    {
+        arr[i] = arr[i - 1]; // Elemente nach rechts verschieben - z.b. arr[6] wird mit arr[5] überschrieben 
+    }
+    arr[0] = tmp; // Erstes Element auf das letzte setzen
+}
